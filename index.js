15b13b199a653e3fd4ad41a4df55b06d2c7dc109
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-07-10 11:51:06 -0500

    added to readme and started index.js code

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const inquirer = require('inquirer')
 const template = require('./src/page-template')
 const writeFile = require('./src/write-file')
+
+// constructor classes and questions for each employee type
+const { Manager, managerQuestionsArr } = require('./lib/Manager');
+const { Engineer, engineerQuestionsArr } = require('./lib/Engineer');
+const { Intern, internQuestionsArr } = require('./lib/Intern');
+// stores team member objects
+const employeesArr = []
